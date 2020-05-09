f73a4e5f3ea4190c0e95c9d7882831d0417bbd7a
Chirag Bhardwaj <46256907+Chiragb9@users.noreply.github.com>
2020-05-09 13:47:39 +0530

    Setup the website. MAde the connection of the website to the blockchain network

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,57 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3';
+import Marketplace from '../abis/Marketplace.json'
+import Navbar from './Navbar'
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      account: "",
+      productCount: 0,
+      products: [],
+      loading: true 
+     }
+  }
+
+  async componentWillMount(){
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3(){
+    if(window.ethereum){
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable()
+    }else if(window.web3){
+      window.web3 = new Web3(window.web3.currentProvider)
+    }else{
+      window.alert("Your broSwser doen't support Ethereum. Try using Metamak!")
+    }
+  }
+
+  async loadBlockchainData(){
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account:accounts[0] });
+    const networkId = await web3.eth.net.getId();
+    const networkData = await Marketplace.networks[networkId]
+    if(networkData){
+      const marketplace = web3.eth.Contract(Marketplace.abi, Marketplace.networks[networkId].address)
+      console.log(marketplace);
+    }else{
+      window.alert("Marketplace not detected on the network");
+    }
+    }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
         <div className="container-fluid mt-5">
+        <Navbar account={this.state.account}/>
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
           </div>
         </div>
       </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+        <nav className="navbar fixed-top flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            target="_blank"
+            href="http://localhost:3000/"
+            rel="noopener noreferrer"
+          >
+            CB Marketplace
+          </a>
+         <ul className="navbar-nav px-3">
+             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+                <small className="badge badge-dark"><span>Account: {this.props.account}</span></small>
+             </li>
+         </ul>
+        </nav>
+         );
+    }
+}
+ 
+export default Navbar;
+
+
